d87d62666fb8f39c3a55643d5650e5546e08815f
MooTaz <61359941+Mootaz-hub@users.noreply.github.com>
2023-02-15 13:27:48 +0100

    Initializing project configurations

---
+++ analysis-service/src/main/java/tn/esprit/AnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class AnalysisServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnalysisServiceApplication.class, args);
+    }
+}
