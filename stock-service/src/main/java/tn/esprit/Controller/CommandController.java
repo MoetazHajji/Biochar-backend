71dc30b63070dffcea8a2b53c6906f1046fb3831
MooTaz <61359941+Mootaz-hub@users.noreply.github.com>
2023-02-18 17:28:08 +0100

    adding Swagger UI Documentation

--- stock-service/src/main/java/tn/esprit/Controller/CommandController.java
+++ stock-service/src/main/java/tn/esprit/Controller/CommandController.java
@@ -1,5 +1,7 @@
 package tn.esprit.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.Entity.Adress;
@@ -8,30 +10,40 @@ import tn.esprit.Interface.ICommandService;
 
 import java.util.List;
 
+@Tag(name = "Command Management")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/command")
 public class CommandController {
     ICommandService commandService;
 
+    @Operation(description = "Add new Command")
     @PostMapping("add")
-    public Command addAdress(@RequestBody Command command){
+    public Command addCommand(@RequestBody Command command){
         return commandService.addCommand(command);
     }
+
+    @Operation(description = "Modify Command")
     @PutMapping("modify")
-    public Command modifyAdress(@RequestBody Command command){
+    public Command modifyCommand(@RequestBody Command command){
         return commandService.modifyCommand(command);
     }
+
+    @Operation(description = "Delete Command")
     @DeleteMapping("delete/{id}")
-    public void deleteAddress(@PathVariable("id") Long id){
+    public void deleteCommand(@PathVariable("id") Long id){
         commandService.deleteCommand(id);
     }
+
+    @Operation(description = "Retreive Command by Id")
     @GetMapping("getAdress/{id}")
-    public Command getAdressById(@PathVariable("id") Long id){
+    public Command getCommandById(@PathVariable("id") Long id){
         return commandService.getCommandById(id);
     }
+
+    @Operation(description = "Retreive all Commands")
     @GetMapping("getAllAdress")
-    public List<Command> getAdressById(){
+    public List<Command> getAllCommand(){
         return commandService.getAllCommands();
     }
 }
--- stock-service/src/main/java/tn/esprit/Controller/SuppliesController.java
+++ stock-service/src/main/java/tn/esprit/Controller/SuppliesController.java
@@ -1,5 +1,7 @@
 package tn.esprit.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.Entity.Supplier;
@@ -11,28 +13,38 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@Tag(name = "Supplies Management")
 @RequestMapping("/supplies")
 public class SuppliesController {
     ISuppliesService suppliesService;
 
+    @Operation(description = "Create new Supplie")
     @PostMapping("add")
-    public Supplies addAdress(@RequestBody Supplies supplies){
+    public Supplies addSupplies(@RequestBody Supplies supplies){
         return suppliesService.addSupplie(supplies);
     }
+
+    @Operation(description = "Modify Supplie")
     @PutMapping("modify")
-    public Supplies modifyAdress(@RequestBody Supplies supplies){
+    public Supplies modifySupplies(@RequestBody Supplies supplies){
         return suppliesService.modifySupplie(supplies);
     }
+
+    @Operation(description = "Delete Supplie")
     @DeleteMapping("delete/{id}")
-    public void deleteAddress(@PathVariable("id") Long id){
+    public void deleteSupplies(@PathVariable("id") Long id){
         suppliesService.deleteSupplie(id);
     }
+
+    @Operation(description = "Retreive Supplie by ID")
     @GetMapping("getAdress/{id}")
-    public Supplies getAdressById(@PathVariable("id") Long id){
+    public Supplies getSuppliesById(@PathVariable("id") Long id){
         return suppliesService.getSupplieById(id);
     }
+
+    @Operation(description = "Retreive all Supplies")
     @GetMapping("getAllAdress")
-    public List<Supplies> getAdressById(){
+    public List<Supplies> getAllSupplies(){
         return suppliesService.getAllSupplies();
     }
 }
