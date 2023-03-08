5ba7ece73ed9bfb267635b88edde8467dbce8380
siwaratiya <99536777+siwaratiya@users.noreply.github.com>
2023-03-09 00:09:33 +0100

    control (#44)

--- analysis-service/src/main/java/tn/esprit/Entity/Medicalcard.java
+++ analysis-service/src/main/java/tn/esprit/Entity/Medicalcard.java
@@ -3,6 +3,7 @@ package tn.esprit.Entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.util.Date;
@@ -20,8 +21,8 @@ public class Medicalcard implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="idMedicalcard")
     private int idMedicalcard;
-    //@NotBlank(message = "card_number obligatoir")
-    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")
+    @NotBlank(message = "card_number obligatoir")
+    @Pattern(regexp = "^[a-zA-Z]{3}-\\d{2}/\\d{2}/\\d{4}$", message = "Le format de la date doit être AAA-11/11/1111")
     private String card_number ;
     private  Date date_creation;
     //@NotBlank(message = "review obligatoir")
